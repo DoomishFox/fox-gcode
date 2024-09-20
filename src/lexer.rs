06ff4ed4e48bf5d5a440c81209b7c10d24976b05
Ashley Moon <42523415+DoomishFox@users.noreply.github.com>
2024-09-19 23:44:07 -0700

    add negative field value lex support plus a couple more g commands (no parse for those yet tho)

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,6 @@
-pub fn lex(command_str: &str) -> Result<Option<Vec::<Field>>, LexError> {
+pub fn lex(command_str: &str) -> Result<Option<Vec<Field>>, LexError> {
     if command_str.starts_with(';') | command_str.trim().is_empty() {
-        return Ok(None)
+        return Ok(None);
     }
 
     let mut scanner = Scanner::new(command_str);
@@ -67,16 +67,16 @@ fn field(scanner: &mut Scanner) -> Result<Option<Field>, LexError> {
             scanner.pop();
             match scanner.scan_numeric() {
                 Some(val) => Ok(Some(Field::M(val as u32))),
-                _ => Err(LexError::NonNumericNextToken)
+                _ => Err(LexError::NonNumericNextToken),
             }
-        },
+        }
         Some('G') => {
             scanner.pop();
             match scanner.scan_numeric() {
                 Some(val) => Ok(Some(Field::G(val as u32))),
-                _ => Err(LexError::NonNumericNextToken)
+                _ => Err(LexError::NonNumericNextToken),
             }
-        },
+        }
         Some('X') => {
             scanner.pop();
             match scanner.scan_numeric() {
@@ -84,14 +84,14 @@ fn field(scanner: &mut Scanner) -> Result<Option<Field>, LexError> {
                 Some(val) => Ok(Some(Field::X(Some(FieldValue::Numeric(val))))),
                 None => Ok(Some(Field::X(None))),
             }
-        },
+        }
         Some('Y') => {
             scanner.pop();
             match scanner.scan_numeric() {
                 Some(val) => Ok(Some(Field::Y(Some(FieldValue::Numeric(val))))),
                 None => Ok(Some(Field::Y(None))),
             }
-        },
+        }
         Some('Z') => {
             scanner.pop();
             match scanner.scan_numeric() {
@@ -99,138 +99,120 @@ fn field(scanner: &mut Scanner) -> Result<Option<Field>, LexError> {
                 None => Ok(Some(Field::Z(None))),
             }
         }
-        _ => Ok(None)
+        _ => Ok(None),
     }
 }
 
 struct Scanner {
     cursor: usize,
-    characters: Vec<char>
+    characters: Vec<char>,
 }
 
 #[allow(dead_code)]
 impl Scanner {
-        pub fn new(string: &str) -> Self {
-            Self {
-                cursor: 0,
-                characters: string.chars().collect(),
+    pub fn new(string: &str) -> Self {
+        Self {
+            cursor: 0,
+            characters: string.chars().collect(),
+        }
+    }
+
+    pub fn cursor(&self) -> usize {
+        self.cursor
+    }
+
+    pub fn peek(&self) -> Option<&char> {
+        self.characters.get(self.cursor)
+    }
+
+    pub fn is_done(&self) -> bool {
+        self.cursor == self.characters.len()
+    }
+
+    pub fn pop(&mut self) -> Option<&char> {
+        match self.characters.get(self.cursor) {
+            Some(character) => {
+                self.cursor += 1;
+                Some(character)
             }
+            None => None,
         }
+    }
 
-        pub fn cursor(&self) -> usize {
-            self.cursor
-        }
-
-        pub fn peek(&self) -> Option<&char> {
-            self.characters.get(self.cursor)
-        }
-
-        pub fn is_done(&self) -> bool {
-            self.cursor == self.characters.len()
-        }
-
-        pub fn pop(&mut self) -> Option<&char> {
+    pub fn scan_numeric(&mut self) -> Option<f32> {
+        let mut sequence = String::new();
+        let mut negative = false;
+        loop {
             match self.characters.get(self.cursor) {
                 Some(character) => {
-                    self.cursor += 1;
-                    Some(character)
-                }
-                None => None,
-            }
-        }
-
-        pub fn scan_numeric(&mut self) -> Option<f32> {
-            let mut sequence = String::new();
-
-            loop {
-                match self.characters.get(self.cursor) {
-                    Some(character) => {
-                        // if the current character is part of a numeric,
-                        // ingest it and move the cursor forward
-                        if character.is_numeric() || character == &'.'{
-                            sequence.push(*character);
-                            self.cursor += 1;
+                    // if the current character is part of a numeric,
+                    // ingest it and move the cursor forward
+                    if character == &'-' {
+                        negative = true;
+                        self.cursor += 1;
+                    } else if character.is_numeric() || character == &'.' {
+                        sequence.push(*character);
+                        self.cursor += 1;
+                    } else {
+                        // otherwise parse the ingested characters
+                        // (if there are any)
+                        break if !sequence.is_empty() {
+                            match negative {
+                                false => Some(sequence.parse::<f32>().unwrap()),
+                                true => Some(sequence.parse::<f32>().unwrap() * -1.0)
+                            }
                         } else {
-                            // otherwise parse the ingested characters
-                            // (if there are any)
-                            break if !sequence.is_empty() {
-                                Some(sequence.parse::<f32>().unwrap())
-                            } else { None }
-                        }
+                            None
+                        };
                     }
-                    None => break None
                 }
+                None => break None,
             }
         }
+    }
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum LexError {
     Generic,
-    NonNumericNextToken
+    NonNumericNextToken,
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum Field {
-    G (u32), // u32 to make the whole process a little easier
-    M (u32),
+    G(u32), // u32 to make the whole process a little easier
+    M(u32),
     // ok so there's a lot of Option<FieldValue> in here but that's
     // because a lot of these fields have variable value types. i'm
     // not particularily happy about this, but it seemed easier than
     // a struct at the time. sorry
-    T (Option<FieldValue>),
-    S (Option<FieldValue>),
-    P (Option<FieldValue>),
-    X (Option<FieldValue>),
-    Y (Option<FieldValue>),
-    Z (Option<FieldValue>),
-    U (Option<FieldValue>),
-    V (Option<FieldValue>),
-    W (Option<FieldValue>),
-    I (Option<FieldValue>),
-    J (Option<FieldValue>),
-    D (Option<FieldValue>),
-    H (Option<FieldValue>),
-    F (Option<FieldValue>),
-    R (Option<FieldValue>),
-    Q (Option<FieldValue>),
-    E (Option<FieldValue>),
-    N (Option<FieldValue>),
-    Ast (u32),
+    T(Option<FieldValue>),
+    S(Option<FieldValue>),
+    P(Option<FieldValue>),
+    X(Option<FieldValue>),
+    Y(Option<FieldValue>),
+    Z(Option<FieldValue>),
+    U(Option<FieldValue>),
+    V(Option<FieldValue>),
+    W(Option<FieldValue>),
+    I(Option<FieldValue>),
+    J(Option<FieldValue>),
+    D(Option<FieldValue>),
+    H(Option<FieldValue>),
+    F(Option<FieldValue>),
+    R(Option<FieldValue>),
+    Q(Option<FieldValue>),
+    E(Option<FieldValue>),
+    N(Option<FieldValue>),
+    Ast(u32),
 }
-
-// TODO: nicer debug printing would be nice but im not going
-// to do it if i need to have a case for every fucking variant
-/*
-impl std::fmt::Debug for Field {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match &self {
-            Field (value) => {}
-        }
-        write!(f, "Field: {:?}{:?}", self., self.arg_v)
-    }
-}
-*/
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum FieldValue {
-    Numeric (f32),
-    String (String),
-    Expr (String),
+    Numeric(f32),
+    String(String),
+    Expr(String),
 }
-
-// TODO: same story with debug printing here as with Field
-/*
-impl std::fmt::Debug for FieldValue {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self.val_t {
-            FieldValueType::Numeric (val) => write!(f, "{:?}(n)", self.val_n.unwrap()),
-            FieldValueType::String => write!(f, "\"{:?}\"(s)", self.val_s),
-            FieldValueType::Expr => write!(f, "{{{:?}}}(e)", self.val_s),
-        }
-    }
-}
-*/
--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,11 @@ pub enum GCommand {
         e: Option<f32>,
         f: Option<f32>,
     },
+    // dwell
+    G4 {
+        p: Option<f32>,
+        s: Option<f32>,
+    },
     // direct stepper move
     G6 {
         a: Option<f32>,
@@ -48,12 +53,84 @@ pub enum GCommand {
         c: Option<f32>,
         r: bool,
     },
+    // retract
+    // technically, reprap will recognize G10 as a set tool offset
+    // command if the P parameter is present. i think this is a dumb
+    // idea so im ignoring it.
+    G10 {
+        s: Option<f32>,
+    },
+    // unretract
+    G11 {
+        s: Option<f32>,
+    },
+    // clean tool
+    G12 {
+        p: Option<u32>,
+        s: Option<f32>,
+        t: Option<u32>,
+        e: Option<u32>,
+    },
+    // G17..19 are CNC specific and i dont want to deal with them.
+    // set units to inches
+    G20,
+    // set units to mm
+    G21,
+    // firmware retract
+    // mostly unsupported
+    G22,
+    // firmware recover
+    // mostly unsupported
+    G23,
+    // mesh validation pattern
+    // marlin only
+    G26 {
+        b: Option<f32>,
+        c: bool,
+        d: bool,
+        h: Option<f32>, // defaults to 205C
+        f: Option<f32>, // defaults to 1.75mm
+        k: bool,
+        l: Option<f32>, // defaults to .20mm
+        o: Option<f32>, // defaults to .3mm
+        p: Option<f32>,
+        q: Option<f32>,
+        r: Option<u32>,
+        s: Option<f32>, // defaults to .4mm
+        //u: uhhhhh???
+        x: Option<f32>,
+        y: Option<f32>,
+    },
+    // park toolhead
+    // marlin only
+    G27 {
+        p: Option<u32>,
+    },
     // move to origin
     G28 {
         x: bool,
         y: bool,
         z: bool,
     },
+    // detailed z-probe
+    // every single fucking firmware has a different goddamn set
+    // of parameters for this stupid command and that's not even
+    // including machinekit which includes *fucking subcommands
+    // for some goddamn reason*.
+    G29,
+    // single z-probe
+    G30 {
+        p: Option<u32>,
+        x: Option<f32>,
+        y: Option<f32>,
+        z: Option<f32>,
+        h: Option<f32>,
+        s: Option<f32>,
+    },
+    // dock probe sled
+    // another multiple implementation type command, im going
+    // with marlin.
+    G31,
     // move to grid point
     G42 {
         i: Option<u32>,
